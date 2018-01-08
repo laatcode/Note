07ece46549d2e699793731b79a83410a65305718
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-01-07 19:58:47 -0500

    Validaciones en vista reset password

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row component-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><b>Reset Password</b></div>
+                <div class="card-header"><b>Cambiar contraseña</b></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.request') }}">
@@ -14,7 +14,7 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 col-form-label">Correo electrónico</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email or old('email') }}" required autofocus>
@@ -28,7 +28,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label">Password</label>
+                            <label for="password" class="col-md-4 col-form-label">Contraseña</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
@@ -42,7 +42,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label">Confirm Password</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label">Confirmar contraseña</label>
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control{{ $errors->has('password_confirmation') ? ' is-invalid' : '' }}" name="password_confirmation" required>
 
@@ -57,7 +57,7 @@
                         <div class="form-group">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Reset Password
+                                    Cambiar contraseña
                                 </button>
                             </div>
                         </div>
