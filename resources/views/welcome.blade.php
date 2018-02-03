1c516ec5cf592cf4753888c765493de1f6f8f5a5
Luis Angel Avila Torres <luisangelavilatorres@gmail.com>
2018-02-03 12:42:45 -0500

    Cambio de fuentes en aplicación y ajuste de texto en página de inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
   <div class="row component-center">
     <div class="jumbotron splash">
       <h1 class="display-4">Notes</h1>
-      <p class="lead">No permitas escapar ninguna de tus ideas, escríbela rapidamente y mantenas disponibles en todo momento con Note.</p>
+      <p class="lead">No permitas escapar ninguna de tus ideas, escríbela rapidamente y mantenas disponibles en todo momento con Notes.</p>
       <hr class="my-4">
       @guest
         <p>¿Aún no tienes una cuenta?</p>
         <p class="lead">
-          <a class="btn btn-primary" href="{{ route('register') }}">Registrarse</a>
+          <a class="btn btn-primary" href="{{ route('register') }}">Crear Cuenta</a>
         </p>
       @else
         <p class="lead">
